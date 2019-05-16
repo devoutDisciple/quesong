38ceca613bad84f2e7a071c4a5c2f111fab6261b
zhangzhen <1094705507@qq.com>
2019-05-16 12:28:02 +0800

    修改我的页面

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # quesong
+#f9f9f9  空白处颜色
+#ebebeb  边框颜色
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,7 @@
 // pages/home/home.js
+const app = getApp();
+
+  
 Page({
 
   /**
@@ -16,6 +19,7 @@ Page({
     duration: 1000
   },
   changeIndicatorDots(e) {
+    
     this.setData({
       indicatorDots: !this.data.indicatorDots
     })
@@ -39,7 +43,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
