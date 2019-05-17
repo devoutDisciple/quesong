0ba92c1df6ebd41a4ef6c47b901f6948718d1de8
zhangzhen <1094705507@qq.com>
2019-05-18 00:37:34 +0800

    修改主页样式

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # quesong
 #f9f9f9  空白处颜色
 #ebebeb  边框颜色
+#8b8b8b  灰色字体
+#ededed  border
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,5 @@
 // pages/home/home.js
 const app = getApp();
-
   
 Page({
 
@@ -8,42 +7,209 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    citys: [
+      {
+        name: "浙江",
+        children: [
+          {
+            name: "杭州",
+            children: [
+              {
+                name: "杭州大学1"
+              },
+              {
+                name: "杭州大学2"
+              },
+              {
+                name: "杭州大学3"
+              }
+            ]
+          },
+          {
+            name: "宁波",
+            children: [
+              {
+                name: "宁波大学1"
+              },
+              {
+                name: "宁波大学2"
+              },
+              {
+                name: "宁波大学3"
+              }
+            ]
+          }
+        ]
+      }
     ],
-    indicatorDots: false,
-    autoplay: false,
-    interval: 5000,
-    duration: 1000
+    campus: ["上海大学1", "上海大学2", "上海大学3", "上海大学4", "上海大学5", "上海大学6"],
+    imgUrls: [
+      'https://quesong.top/attachment/images/2/2019/05/TBT4K9NbZu4KbV02vKxvkRN4D7B0Y6.jpg',
+      'https://quesong.top/attachment/images/2/2019/05/x9x2652Ng7K255Dh5q6F5xqCwX5xh8.jpg',
+      'https://quesong.top/attachment/images/2/2019/05/DupPq4LuslujS2Ws2uETSWkwu8UfKu.jpg',
+      'https://quesong.top/attachment/images/2/2019/05/sU2ruU5USkH45LH5OZ2Uxz6lb1uR28.jpg',
+      'https://quesong.top/attachment/images/2/2019/05/r4lYlN3mOf3z4Oo3z9TO3o9g7mNn3l.jpg',
+      'https://quesong.top/attachment/images/2/2019/04/XUll8nDpp68oy88NVIV66nL8LPWpwE.jpg'
+    ],// 轮播图url
+    positionDialogVisible: false, //位置弹框的开关
+    interval: 5000, // 轮播图间隔时间
+    duration: 1000, // 轮播图延迟时间
+    goodsNum: 50, // 商品数量
+    freeGoodsNum: 10,// 免费商品数量
+    imgUrl: "https://quesong.top/attachment/images/2/2019/05/Cej3XdyZFkYp1pxqQYjfJMzS1jY3yZ.jpeg", // 商品图片
+    // 商品种类
+    type: [
+      {
+        name: "美食",
+        icon: "star-o",
+        color: "red"
+      },{
+        name: "美食",
+        icon: "star-o",
+        color: "red"
+      },{
+        name: "美食",
+        icon: "star-o",
+        color: "red"
+      },{
+        name: "美食",
+        icon: "star-o",
+        color: "red"
+      },{
+        name: "美食",
+        icon: "star-o",
+        color: "red"
+      },{
+        name: "美食",
+        icon: "star-o",
+        color: "red"
+      },{
+        name: "美食",
+        icon: "star-o",
+        color: "red"
+      },
+    ],
+    // 位置按钮种类
+    columns: [
+    ],
+    position: ""
   },
-  changeIndicatorDots(e) {
-    
+  // 位置弹框的开关
+  onShowPositionDialog() {
     this.setData({
-      indicatorDots: !this.data.indicatorDots
+      positionDialogVisible: !this.data.positionDialogVisible
     })
   },
-  changeAutoplay(e) {
+  // 改变位置的时候
+  onChangePosition(event) {
+    console.log(event.detail, 111)
+    const { picker, value, index } = event.detail;
+    // picker.setColumnValues(1, citys[value[0]]);
+    if(index == 0) {
+      let citys = this.citys;
+      let selectKey = value[0];
+      citys.map(item => {
+        if(item.name == selectKey) {
+
+        }
+      })
+    }
+  },
+  // 选取位置取消
+  onCancelPosition(event) {
+    console.log(event)
+    // 关闭弹框
+    this.onShowPositionDialog();
+  },
+  // 选取位置确定
+  onConfirmPosition(event) {
+    console.log(event.detail.value)
+    // 关闭弹框
+    this.onShowPositionDialog();
     this.setData({
-      autoplay: !this.data.autoplay
+      position: event.detail.value[2]
     })
   },
-  intervalChange(e) {
-    this.setData({
-      interval: e.detail.value
+  // 点击商店
+  shopClick(e) {
+    let name = e.currentTarget.dataset.name;
+    console.log(e.currentTarget.dataset.name);
+    // 跳转到详情页
+    wx.navigateTo({
+      url: '../shopDetail/shopDetail',
+      success: function() {
+        // 设置标题
+        wx.setNavigationBarTitle({
+          title: name
+        })
+        // 设置导航栏颜色
+        wx.setNavigationBarColor({
+          frontColor: "#ffffff",//前景颜色值
+          backgroundColor: "#333"//背景颜色值
+        })
+      }
     })
   },
-  durationChange(e) {
-    this.setData({
-      duration: e.detail.value
-    })
-  },
+  // 按条件遍历
+  // circulationByCondition(citys, columns, name) {
+  //   let column = {
+  //     values: []
+  //   };
+  //   citys.map((item,index) => {
+  //     column.values.push(item.name);
+  //     if(name) {
+  //       if(item.children && item.children.length != 0 && item.name == name) {
+  //         this.circulation(item.children, columns, "")
+  //       }
+  //     } else {
+  //       if(item.children && item.children.length != 0 && index == 0) {
+  //         this.circulation(item.children, columns, "")
+  //       }
+  //     }
+      
+  //   })
+  //   this.circulation(item.children, columns)
+  //   return columns;
+  // },
+
+  // // 遍历城市
+  // circulation(citys, columns) {
+  //   let column = {
+  //     values: []
+  //   };
+  //   citys.map((item,index) => {
+  //     column.values.push(item.name);
+  //     if(item.children && item.children.length != 0 && index== 0) {
+  //       this.circulation(item.children, columns)
+  //     }
+  //   })
+  //   columns.unshift(column);
+  //   return columns;
+  // },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     
+    // {
+    //   values: ['福州', '厦门', '莆田', '三明', '泉州'],
+    //   className: 'column2'
+    // },
+    let columns = [
+      {
+        values: ["上海"],
+        className: 'column1'
+      },
+      {
+        values: ["上海"],
+        className: 'column2'
+      },
+      {
+        values: this.data.campus,
+        className: 'column2'
+      },
+    ];
+    this.setData({columns, position: this.data.campus[0]});
   },
 
   /**
@@ -100,35 +266,10 @@ Page({
 
   },
 
-  onTap: function() {
-    wx.showTabBarRedDot({
-      index: 0,
-      text: 1
-    })
-    wx.showNavigationBarLoading();
-    wx.showToast({
-      title: '成功',
-      icon: 'success',
-      duration: 2000
-    })
-    wx.showLoading({
-      title: "hello world"
-    })
-    wx.showActionSheet({
-      itemList: ['A', 'B', 'C'],
-      success(res) {
-        console.log(res.tapIndex)
-      },
-      fail(res) {
-        console.log(res.errMsg)
-      }
-    })
-  },
-
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    console.log(124)
   }
 })
